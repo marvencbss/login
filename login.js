4941975518cd41bb79feaf359f461a903fa63e05
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-07-06 19:42:38 -0300

    .

--- login.js
+++ login.js
@@ -3,14 +3,26 @@ const Btlogin = document.querySelector('#Btlogin');
 Btlogin.addEventListener('click', login);
 
 function login() {
-    const valorLogin = document.querySelector('#txLogin');
+    const txLogin = document.querySelector('#txLogin');
     const ValLogin = txLogin.value;
+
+    const txSenha = document.querySelector('#txSenha')
+    const valSenha = txSenha.value;
     validaLogin(ValLogin);
+
+
+const ehValido = validaLogin(ValLogin, valSenha);
+
+if(ehValido) {
+    alert('Válido');
+} else {
+    alert('Inválido');
+}
 }
 
-function validaLogin(umValorLogin) {
+function validaLogin(umValorLogin, UmValorSenha) {
 
-    if(umValorLogin=='eu') {
+    if (umValorLogin=='eu' && UmValorSenha=='abcde') {
         return true;
     } else {
         return false;
